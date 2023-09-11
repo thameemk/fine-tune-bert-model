c9ccbfb4e9d03cef9cfa88982df548d92b2ec967
thameemk <thameemk612@gmail.com>
2023-09-11 23:02:02 +0530

    Change: Build Dataset [ Custom Dataset Generator For Fine-Tuning The Model]

--- src/data/build_dataset.py
+++ src/data/build_dataset.py
@@ -1,13 +1,29 @@
-# Data Source
-
-# Sample dataset (text) generator using wikipedia
+# Build a custom dataset for fine-tuning
 
 
-import wikipedia
+def generate_custom_dataset() -> list[dict]:
+    """
 
-from src.data.text_preprocessing import text_preprocessing
+    Returns: List[Dict] -  Examples: [{'text': 'write for browse play', 'label': 'write'}, {'text': 'call for write
+    browse learn', 'label': 'call'}]
+    """
+    import random
 
+    # List of example verbs
+    verbs = ["search", "send", "read", "write", "calculate", "browse", "translate", "call", "play", "learn"]
 
-def get_content_from_wikipedia(title: str) -> str:
-    raw_content = wikipedia.summary(title)
-    return text_preprocessing(raw_content)
+    # Generate 1000 random input and output pairs
+    input_texts = []
+    output_words = []
+
+    for _ in range(1000):
+        input_text = random.choice(verbs) + " for " + " ".join(random.sample(verbs, random.randint(1, 3)))
+        output_word = input_text.split()[0]
+
+        input_texts.append(input_text)
+        output_words.append(output_word)
+
+    # Print the generated pairs
+
+    dataset = [{'text': input_texts[i], 'label': output_words[i]} for i in range(len(input_texts))]
+    return dataset
