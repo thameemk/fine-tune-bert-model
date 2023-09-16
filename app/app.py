558304bebeae8a7c0ee11eafae10c705939c222d
thameemk <thameemk612@gmail.com>
2023-09-16 23:21:34 +0530

    change: app

--- app/app.py
+++ app/app.py
@@ -5,11 +5,10 @@ from libs.transformers_ import load_model_and_tokenizer
 from models.documents import ActionAndText, RecognizeActionInput
 
 app = FastAPI()
+model, tokenizer = load_model_and_tokenizer('thameemk/actions-recognizer', 3)
 
 
 @app.post("/recognize_action/")
 def read_root(text: RecognizeActionInput) -> ActionAndText:
-    # todo - load model on app starting
-    model, tokenizer = load_model_and_tokenizer('thameemk/actions-recognizer', 3)
     res = recognize_action(model, tokenizer, text.text)
     return res
