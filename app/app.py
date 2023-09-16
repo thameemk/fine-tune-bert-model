a9333978f69040af5bd25fae03cd01aa4d5fd622
thameemk <thameemk612@gmail.com>
2023-09-16 21:01:36 +0530

    change: fastapi app

--- app/app.py
+++ app/app.py
@@ -1,25 +1,13 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-
-class SearchQuery(BaseModel):
-    query: str
-    context: str | None = None
-
-
-class SearchQueryResponse(BaseModel):
-    query: str
-    result: str
-    context: str | None = None
-
+from models.documents import ActionAndText, RecognizeActionInput
 
 app = FastAPI()
 
 
-@app.post("/query/")
-def read_root(search_query: SearchQuery) -> SearchQueryResponse:
-    return SearchQueryResponse(
-        query=search_query.query,
-        context=search_query.context,
-        result=""
+@app.post("/recognize_action/")
+def read_root(text: RecognizeActionInput) -> ActionAndText:
+    return ActionAndText(
+        text=text.query,
+        action=""
     )
