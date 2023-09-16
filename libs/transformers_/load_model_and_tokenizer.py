6a7c7294f0599d3ad45c08cefa11dea1bf8b74e5
thameemk <thameemk612@gmail.com>
2023-09-16 22:56:40 +0530

    change: HUGGING_FACE_ACCESS_TOKEN

--- libs/transformers_/load_model_and_tokenizer.py
+++ libs/transformers_/load_model_and_tokenizer.py
@@ -1,8 +1,11 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
+from settings import HUGGING_FACE_ACCESS_TOKEN
+
 
 def load_model_and_tokenizer(model_name: str, num_labels: int):
-    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
-    tokenizer = AutoTokenizer.from_pretrained(model_name, num_labels=num_labels)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=num_labels,
+                                                               token=HUGGING_FACE_ACCESS_TOKEN)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, num_labels=num_labels, token=HUGGING_FACE_ACCESS_TOKEN)
 
     return model, tokenizer
